6ec3a75112eb10842a704781e74752efb41aa6a9
PaulGaona <paulrgaona@gmail.com>
2025-04-21 17:31:37 -0700

    update for small sims

---
+++ scripts_small.R
@@ -0,0 +1,16 @@
+# functions and packages
+source(".//funs.R")
+source("./packages.R")
+# script for running analysis
+# change when needed to
+#source("./static_sim_and_setting.R")
+#source("./large_multi_sims.R")
+source("./multi_sims.R")
+# save mse as rda file
+#save(mse.all, file = "mse.all.rda")
+# save lasso covariates as rda file
+#save(cov.kept.list, file = "lasso.cov.kept.rda")
+
+# results from multi_sims
+#save(large.cov.large.n, file = "large.cov.large.n.rda")
+save(small.cov.small.n2, file = "small.cov.small.n2.rda")
