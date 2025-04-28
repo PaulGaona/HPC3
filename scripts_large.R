f04cf1b3bd744dd3612a3b0a15af6b6bb00fdc24
PaulGaona <paulrgaona@gmail.com>
2025-04-27 17:37:45 -0700

    Update scripts_large.R

--- scripts_large.R
+++ scripts_large.R
@@ -2,7 +2,7 @@
 source(".//funs.R")
 source("./packages.R")
 # script for running analysis
-source("./med_multi_sims.R")
+source("./large_multi_sims.R")
 
 # results from multi_sims
 save(large.cov.large.n, file = "large.cov.large.n2.rda")
